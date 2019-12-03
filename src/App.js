248a41d755eacc7a29bfe124e2f5391583d7049d
ArtemFedoseev <58297238+ArtemFedoseev@users.noreply.github.com>
2019-12-03 09:26:05 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hours: 24,
+            date: new Date()
+        }
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    handleClick(hoursSet){
+        this.setState(state => ({
+            hours: hoursSet
+        }));
+    }
+
+    render() {
+        const format = this.state.hours;
+        const options = {
+            hour12 : format === 12
+        };
+        return (
+            <div>
+                <h1>{this.state.date.toLocaleTimeString("en-US",options)}.</h1>
+                <div className="radio">
+                    <p>Format:</p>
+                    <label>
+                        <input type="radio" checked={this.state.hours === 24} onClick={_ => this.handleClick(24)} />
+                        <span style={{cursor:'pointer'}} onClick={_ => this.handleClick(24)}> 24 </span>
+                    </label>
+                </div>
+                <div className="radio">
+                    <label>
+                        <input type="radio"  checked={this.state.hours === 12} onClick={_ => this.handleClick(12)} />
+                        <span style={{cursor:'pointer'}} onClick={_ => this.handleClick(12)}> 12 </span>
+                    </label>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+
+
+export default Clock;
